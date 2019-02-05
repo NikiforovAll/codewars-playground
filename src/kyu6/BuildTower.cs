977cc45e6196473858d6ebf95a18f9a1cecbe27c
NikiforovAll <nikiforovalekcey@gmail.com>
2019-02-05 10:20:32 +0200

    codewars project init

---
+++ src/kyu6/BuildTower.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace CodeWars.Kyu6.BuildTower
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/build-tower/
+    /// </summary>
+    
+    using System;
+    using System.Text;
+    public class Kata
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            int baseLength = 1 + 2 * (nFloors - 1);
+            StringBuilder tree = new StringBuilder(new String(' ', baseLength));
+            var result = new string[nFloors];
+            int padSize = nFloors - 1;
+            for (int floor = 1; floor <= nFloors; floor++)
+            {
+                tree.Replace(' ', '*', padSize, 1 + 2 * (floor - 1));
+                result[floor - 1] = tree.ToString();
+                padSize--;
+            }
+            return result;
+        }
+    }
+}
---
+++ tests/BuildTowerTests.cs
@@ -0,0 +1,22 @@
+
+using CodeWars.Kyu6.BuildTower;
+using NUnit.Framework;
+using System;
+
+namespace Kyu6.Tests
+{
+    namespace Solution
+    {
+        [TestFixture]
+        public class KataTests
+        {
+            [Test]
+            public void BasicTests()
+            {
+                Assert.AreEqual(string.Join(",", new[] { "*" }), string.Join(",", Kata.TowerBuilder(1)));
+                Assert.AreEqual(string.Join(",", new[] { " * ", "***" }), string.Join(",", Kata.TowerBuilder(2)));
+                Assert.AreEqual(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", Kata.TowerBuilder(3)));
+            }
+        }
+    }
+}
