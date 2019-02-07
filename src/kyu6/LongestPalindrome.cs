58c12d9d98409152e6c093e052d854bf36115df7
NikiforovAll <nikiforovalekcey@gmail.com>
2019-02-07 19:15:45 +0200

    longest palindrome set up

---
+++ src/kyu6/LongestPalindrome.cs
@@ -0,0 +1,17 @@
+namespace CodeWars.Kyu6.LongestPalindrome
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/longest-palindrome/
+    /// </summary>
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Kata
+    {
+        public static int GetLongestPalindrome(string str)
+        {
+            return 0;
+        }
+    }
+}
---
+++ tests/LongestPalindromeTests.cs
@@ -0,0 +1,30 @@
+namespace ky6
+{
+    using CodeWars.Kyu6.LongestPalindrome;
+
+    namespace Kyu6.LongestPalindromeTests
+    {
+        namespace Solution
+        {
+            using NUnit.Framework;
+            using System;
+
+            [TestFixture]
+            public class SolutionTest
+            {
+                [TestCase("", ExpectedResult = 0, Description = "empty string test")]
+                [TestCase(null, ExpectedResult = 0, Description = "'null' value test")]
+                [TestCase("a", ExpectedResult = 1, Description = "'a' value test")]
+                [TestCase("aa", ExpectedResult = 2, Description = "'aa' value test")]
+                [TestCase("baa", ExpectedResult = 2, Description = "'baa' value test")]
+                [TestCase("abc0cba1", ExpectedResult = 7, Description = "'abc0cba1' value test")]
+                [TestCase("12 21glg", ExpectedResult = 5, Description = "'12 21glg' value test")]
+                [TestCase("   ", ExpectedResult = 3, Description = "empty space test")]
+                public int SampleTest(string str)
+                {
+                    return Kata.GetLongestPalindrome(str);
+                }
+            }
+        }
+    }
+}
