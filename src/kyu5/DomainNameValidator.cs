6a88de4acd1f7531461f1cc99bed435c7233bb0e
NikiforovAll <nikiforovalekcey@gmail.com>
2020-02-20 16:06:07 +0200

    DomainNameValidator

--- src/kyu5/DomainNameValidator.cs
+++ src/kyu5/DomainNameValidator.cs
@@ -1,13 +1,30 @@
+using System.Linq;
 using System;
 namespace CodeWars.Kyu5.DomainNameValidator
 {
     public class DomainNameValidator
     {
-        private const string DomainSeparator = ".";
+        private const char DomainSeparator = '.';
+        private const char MinusCharacter = '-';
         public bool validate(string domain)
         {
-            return false;
+            var levelNames = domain.Split(DomainSeparator);
+            Predicate<string>[] domainLevelValidators = new Predicate<string>[] {
+                (string d) => d.Length <= 253,
+            };
+            Predicate<string[]>[] levelNameListValidators = new Predicate<string[]>[] {
+                (string[] lns) => lns.Length <= 127 && lns.Length > 1,
+                (string[] lns) => !Int32.TryParse(lns.Last(), out _)
+            };
+            Predicate<string>[] levelNameValidators = new Predicate<string>[] {
+                (string ln) => ln.Length > 0 && ln.Length < 64,
+                (string ln) => !ln.StartsWith(MinusCharacter) && !ln.EndsWith(MinusCharacter),
+                (string ln) => ln.All(c =>
+                    (Char.IsLetterOrDigit(c) && (c < 128))|| c == MinusCharacter)
+            };
+            return domainLevelValidators.All(p => p.Invoke(domain))
+                && levelNameListValidators.All(p => p.Invoke(levelNames))
+                && levelNames.All(ln => levelNameValidators.All(p => p.Invoke(ln)));
         }
-
     }
 }
--- tests/DomainNameValidatorTest.cs
+++ tests/DomainNameValidatorTest.cs
@@ -1,6 +1,5 @@
 namespace CodeWars.DomainNameValidator
 {
-    using System;
     using NUnit.Framework;
     using CodeWars.Kyu5.DomainNameValidator;
     [TestFixture]
@@ -11,15 +10,15 @@ namespace CodeWars.DomainNameValidator
         [Test]
         public void DomainTests()
         {
-            // Assert.AreEqual(false, v.validate("codewars"));
-            // Assert.AreEqual(true, v.validate("g.co"));
-            // Assert.AreEqual(true, v.validate("codewars.com"));
-            // Assert.AreEqual(true, v.validate("CODEWARS.COM"));
-            // Assert.AreEqual(true, v.validate("sub.codewars.com"));
-            // Assert.AreEqual(false, v.validate("codewars.com-"));
-            // Assert.AreEqual(false, v.validate(".codewars.com"));
-            // Assert.AreEqual(false, v.validate("example@codewars.com"));
-            // Assert.AreEqual(false, v.validate("127.0.0.1"));
+            Assert.AreEqual(false, v.validate("codewars"));
+            Assert.AreEqual(true, v.validate("g.co"));
+            Assert.AreEqual(true, v.validate("codewars.com"));
+            Assert.AreEqual(true, v.validate("CODEWARS.COM"));
+            Assert.AreEqual(true, v.validate("sub.codewars.com"));
+            Assert.AreEqual(false, v.validate("codewars.com-"));
+            Assert.AreEqual(false, v.validate(".codewars.com"));
+            Assert.AreEqual(false, v.validate("example@codewars.com"));
+            Assert.AreEqual(false, v.validate("127.0.0.1"));
         }
     }
 }
